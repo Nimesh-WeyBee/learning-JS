196f2a661217e5d28a6a87f58dc6219ccb8baef5
Nimesh Kadecha <nimesh@weybee.in>
2025-07-10 17:48:07 +0530

    completed the pig game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,73 @@
 'use strict';
+
+// const message = document.querySelector('.message');
+
+// message.textContent = 'Correct number 🎉';
+
+// document.querySelector('.number').textContent = 13;
+
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 0;
+
+(() => {
+  let secreatNumber = Math.trunc(Math.random() * 20) + 1;
+  let score = 20;
+  let highscore = 0;
+
+  const message = document.querySelector('.message');
+
+  const numberEle = document.querySelector('.number');
+  const guessEle = document.querySelector('.guess');
+  const scoreEle = document.querySelector('.score');
+  const highScoreEle = document.querySelector('.highscore');
+  const bodyEle = document.querySelector('body');
+
+  const checkBtn = document.querySelector('.check');
+  const againBtn = document.querySelector('.again');
+
+  const changeScore = score => {
+    if (score > 0) scoreEle.textContent = score;
+    else {
+      message.textContent = ' 💥 You lost the Game ';
+    }
+  };
+
+  const displayMessage = msg => {
+    message.textContent = msg;
+  };
+
+  checkBtn.addEventListener('click', () => {
+    const guess = Number(guessEle.value);
+
+    if (!guess) {
+      displayMessage('⛔ No number');
+    } else if (guess == secreatNumber) {
+      displayMessage('Correct number 🎉');
+
+      if (highscore < score) {
+        highScoreEle.textContent = score;
+        highscore = score;
+      }
+
+      bodyEle.style.backgroundColor = '#60b347';
+      numberEle.style.width = '30rem';
+
+      numberEle.textContent = secreatNumber;
+    } else {
+      displayMessage(`${guess < secreatNumber ? '📉 Too low' : '📈 Too High'}`);
+      changeScore(--score);
+    }
+  });
+
+  againBtn.addEventListener('click', () => {
+    score = 20;
+    changeScore(score);
+    displayMessage('start guessing...');
+    guessEle.value = '';
+    bodyEle.style.backgroundColor = '#222';
+    numberEle.style.width = '15rem';
+    numberEle.textContent = '?';
+    secreatNumber = Math.trunc(Math.random() * 20) + 1;
+  });
+})();
--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const toggleModel = () => {
+  modal.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+};
+
+btnOpenModal.forEach(e => {
+  e.addEventListener('click', toggleModel);
+});
+
+btnCloseModal.addEventListener('click', toggleModel);
+overlay.addEventListener('click', toggleModel);
+
+window.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) toggleModel();
+});
