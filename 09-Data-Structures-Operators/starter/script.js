6bc84f35ab6c8f45fcbdede18f0c2b320ab6dba8
Nimesh Kadecha <nimesh@weybee.in>
2025-07-14 15:28:01 +0530

    completed strings

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 const italianFoods = new Set([
   'pasta',
@@ -53,6 +51,28 @@ const restaurant = {
   },
 };
 
+const restaurent2 = {
+  tue: {
+    open: '09:00',
+    close: '21:00',
+  },
+  wed: {
+    open: '09:00',
+    close: '21:00',
+  },
+  thu: {
+    open: '09:00',
+    close: '21:00',
+  },
+  fri: {
+    open: '09:00',
+    close: '21:00',
+  },
+};
+
+// console.log(Object.entries(restaurent2));
+
+// CHALLENGE - 1 -------------------------------------------------
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -108,12 +128,60 @@ function printGoals(...playerNames) {
   console.log(...playerNames, playerNames.length);
 }
 
+// CHALLENGE - 2 -------------------------------------------------
+
+for (const [index, Gname] of game.scored?.entries()) {
+  // console.log(`Goal: ${index + 1}: ${Gname}`);
+}
+
+let sum = 0;
+for (const c of Object.values(game.odds)) {
+  sum += c;
+}
+sum /= Object.values(game.odds).length;
+// console.log('Average odds : ' + sum);
+
+let outputStr = '';
+for (const [teamName, odds] of Object.entries(game.odds)) {
+  outputStr += `odd of${(game?.[teamName] && ' victory') ?? ''} ${
+    game?.[teamName] ?? 'draw'
+  } : ${odds} \n`;
+}
+
+// console.log(outputStr);
+
+game.scores = {};
+for (const c of game.scored) {
+  game.scores[c] = game.scores?.[c] ? game.scores[c] + 1 : 1;
+}
+// console.log(game.scores);
+
+const arr = ['a', 'a', 'a', 'b', 'c', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+const set = new Set(arr);
+
+const rest = new Map();
+
+const variable = new Map([
+  ['key', 'val'],
+  ['key', 'val'],
+  ['key', 'val'],
+  ['key', 'val'],
+  ['key', 'val'],
+]);
+
+// console.log(set);
+
+// console.log(set.add('z').delete('a'));
+// console.log(set.delete('z'));
+// console.log(set);
+
 // printGoals('a');
 // printGoals('a', 'b');
 // printGoals('a', 'b', 'c', ...game.scored);
 
-team1 < team2 && console.log('team 1 win');
-team1 > team2 && console.log('team 2 win');
+// team1 < team2 && console.log('team 1 win');
+// team1 > team2 && console.log('team 2 win');
 
 // const rest1 = {
 //   name: 'Capri',
@@ -211,3 +279,94 @@ team1 > team2 && console.log('team 2 win');
 
 // const [d = 1, e = 1, f = 1] = [8, 9];
 // console.log(d, e, f);
+
+// CHALLENGE- 3 ----------------------------------------------------------------
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+const events = [...new Set(gameEvents.values())];
+// console.log(events);
+
+gameEvents.delete(64);
+// console.log(gameEvents);
+
+// console.log([...gameEvents.keys()].pop() / gameEvents.size);
+
+for (const [time, event] of gameEvents) {
+  // console.log(`[${time <= 45 ? 'FIRST HALF' : 'SECOND HALF'}] : ${event}`);
+}
+
+const airline = 'Indian airline';
+const plain = 'A320';
+
+const checkMiddleSeat = function (seat) {
+  //B & E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat');
+  else console.log('You got lucky');
+};
+
+// checkMiddleSeat('11B');
+// checkMiddleSeat('23C');
+// checkMiddleSeat('3E');
+
+// console.log('Hi hello hi hi hi'.replaceAll('hi', 'hey'));
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+/*
+
+underscore_case;
+ first_name;
+Some_Variable;
+ calculate_AGE;
+delayed_departure;
+
+*/
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const inp = text.toLowerCase().trim().split('\n');
+
+  for (const [i, v] of inp.entries()) {
+    const [temp1, temp2] = v.split('_');
+    let ans = temp1.trim() + temp2.replace(temp2[0], temp2[0].toUpperCase());
+
+    ans = ans.padEnd(20);
+
+    // console.log(`${ans}${"💥".repeat(i+1)} `);
+  }
+});
+
+// EXTRA CHALLENGE
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const allFlightArr = flights.split('+');
+
+for (const v of allFlightArr) {
+  // console.log(v.split(';'));
+  let [status, from, to, time] = v.split(';');
+  status = status.replaceAll('_', ' ');
+  status = status.startsWith(' Delayed') ? '🔴' + status : status;
+  // console.log(status);
+  from = from.slice(0, 3).toUpperCase();
+  to = to.slice(0, 3).toUpperCase();
+
+  const ans = `${status} from ${from} to ${to} (${time.replace(':','h')})`
+
+  console.log(`${ans.padStart(45)}`);
+}
